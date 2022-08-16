e9c6eefaed02d28fa8d1ed3dfa71cedbe9c94a05
Florencia Togliefoso <florenciatogliefoso@gmai.com>
2022-08-15 22:09:36 -0300

    cambios

--- index.js
+++ index.js
@@ -1,109 +1,4 @@
-/* alert('Bienvenidos a mi pagina web')
-
- alert("Espero les guste Messi")
-
- alert('Gracias por visitar')
-
- const esEstudiante = true;
-const esProfesor = false;
-
-const valor = true; 
-console.log(!valor)
-
-console.log(Boolean(0) )
-console.log(Boolean("10"))
-
-console.log("9" == 9)
-
-console.log(10 === 10)
-console.log(10 === "10")
-
-console.log(10 == 10)
-console.log(10 == "10")
-console.log(10 == "5")
-
-console.log(10 > 10)
-console.log(10 > 5)
-console.log(10 > 15)
-
-console.log(10 < 50)
-console.log(10 < 5)
-
-console.log(10 >= 10)
-console.log(10 <= 5)
-
-console.log(10 !== 10)
-console.log(10 !== 5)
-
-console.log(10 != "10")
-console.log(10 != 5)
-
-const edadDelUsuario = prompt("Ingrese su edad")
-
-const body = document.querySelector("body")
-body.style.display = "none"
-
-if(edadDelUsuario >= 18){
-    body.style.display = "flex"
- } else { 
-    alert("No podes ingresar")
- }
-
- const año = prompt("Ingrese un año en el cual Messi salio campeon")
- 
- if(año === "2014" || año === "2015"){ 
-    console.log("Fue campeon")
- } 
- else if (año ==="2022" || año === "2021"){
-    console.log("Fue campeon")
- }
-
- if(año === "2018" || año === "2019"){ 
-    console.log("Fue campeon")
-}
-
-if(año === "2016" || año === "2017"){ 
-    console.log("Fue campeon")
-}
-
- else { 
-    console.log("Ese año no salio campeon")
- }
-
- console.log("Hola Fanaticos de Messi")
-
-function saludar (nombre){
-    return nombre
-}
-
-const comprobarContrasenia = (contrasenia) => {
-    if(contrasenia.length >=10){
-        return true 
-    } else{
-        return false
-    }
-}
-
-console.log(comprobarContrasenia("messi100721"))
-
-const checkearContrasenia = (contrasenia) => contrasenia.length >= 10
-
-console.log(checkearContrasenia("messiCampeon1007"))
-
-console.log("messiCampeon1007".length >= 10)
- 
-dato1 = prompt("Ingrese el año de nacimiento de Leo: ");
-dato2 = prompt("Ingrese el año en el que debuto: ");
-
-function comparacion(dato1, dato2){
-    if(dato1.length == dato2.length){
-        console.log("1987")
-    } else{
-        console.log("2004")
-    }
-}
-
-const argumentos=(argumento1, argumento2)=>{
+/*const argumentos=(argumento1, argumento2)=>{
     if(argumento1.length == argumento2.length){
         return `Si, ese año nacio`
     } else {
@@ -149,28 +44,6 @@ console.log( "Carola".lenght )
 const  fruta  =  "manzana"
 console.log ( "123120'9310'293013-_-´+´{+´pásodsa´+dp+p65469*-/*-" . longitud )
 console.log ( fruta.length )
-
-console.log(fruta.lenght)
-
-const  frutas  =  [  "plátano" ,  "manzana" ,  "pera" ,  "uva" ,  "melón" , "sandía  "  ]
-
-console.log ( frutas.lenght )
-
-for  ( let  i  =  0 ;  i  <  frutas.lenght;  i ++ ) {
-    console.log ( `En la posición ${ i } , se encuentra: ${ frutas [ i ] } ` )
-} 
-const  arrayRaro  =  [ 1 ,  2 ,  3  , 4 ,  "carola" ,  "Kojima" ,  [ 1 ,  2  , 3 ,  4 ,  5 ] ,  true ,  false ,  { nombre : "Fernando" }  ]
-
-const  arraysConArrays  =  [  [  1 ,  2 ,  3  ] ,  [  4 ,  5 ,  6  ] ,  [  7 ,  8 ,  9  ]  ]
-
-for  ( let i  =  0 ;  i  <  arraysConArrays . longitud  ;  i ++ ) {
-    console.log( arraysConArrays [ i ] )
-
-    for (  let  j  =  0 ;  j  <  arreglosConArrays [ i ] . longitud ;  j ++ ) {
-        console.log ( arraysConArrays [ i ] [ j ] )
-    }
-}
-
 const clientes=  [
     {
         nombre : "Carola" ,
@@ -205,68 +78,12 @@ for  ( let  i  =  0 ;  i  <  clientes . longitud ;  i ++  ) {
     console.log ( clientes [ i ] . nombre )
 }
 
-console.log ( frutas ) 
-console.log ( frutas . push ( "papaya" ) )  
-console.log ( frutas ) 
 
 const  edad  =  20
 let  otraEdad  =  edad
 otraEdad  =  27
 
-const  otrasFrutas  =  frutas
-otrasFrutas.push ( "cereza" )
-console.log ( otrasFrutas )
-console.log ( frutas )
 
-const  nuevasFrutas  =  [... frutas]
-nuevasFrutas.push("tomate")
-
-console.log ( frutas )
-console.log( nuevasFrutas )
-
-nuevasFrutas.unshift ( "mandarina" )
-console.log(nuevasFrutas)
-nuevasFrutas.unshift ( "POMELO" ,  "TOMATE" )
-console.log ( nuevasFrutas )
-
-nuevasFrutas.pop ( )
-console.log( nuevasFrutas ) 
-nuevasFrutas.pop( )
-nuevasFrutas.pop ( )
-nuevasFrutas.pop ( )
-nuevasFrutas.pop ( )
-nuevasFrutas.pop ( )
-console.log ( nuevasFrutas )
-
-
-console.log ( nuevasFrutas )
-nuevasFrutas.reverse ( )
-console.log(  nuevasFrutas )
-
-console.log ( nuevasFrutas.join ( ) )
-console.log ( nuevasFrutas.join ( " " ) )
-console.log ( nuevasFrutas .join( ", " ) )
-console.log ( nuevasFrutas )
-
-const  verduras  =  [  "acelga" ,  "espinaca" ,  "perejil" ,  "cilantro" ,  "lechuga" ]
-const  nuevoArray  =  nuevasFrutas . concat ( verduras )  
-console.log ( nuevoArray )
-
-const  otrasVerduras  =  verduras.slice ( ) 
-const  verdurasQueMeGustan  =  verduras.slice ( 3 ,  4 )  
-
-console.log ( verduras )
-console.log ( verdurasQueMeGustan )
-
-verduras.splice ( 0 ,  0 ,  "zapallito" )
-console.log ( verduras )
-verduras.splice( 3 ,  1 )
-console.log ( verduras )
-
-console.log ( verduras.indexOf ( "zapallito" ) )
-
-console.log ( verduras.includes ( "perejil" ) )
-console.log ( verduras.includes( "cilantro" ) )
 
 const  carteraDeClientes  =  [ ]
 
@@ -280,11 +97,6 @@ const  personita  =  {
 console.log ( carteraDeClientes )
 carteraDeClientes.push( personita )
 console.log ( carteraDeClientes )
-
-
-const saludo = saludar()
-
-console.log(saludo)
 
 let edadLegal = 18
 
@@ -460,7 +272,7 @@ console.log(fecha.toDateString())
 console.log(fecha.toLocaleDateString())
 console.log(fecha.toLocaleString())
 console.log(fecha.toTimeString())
-*/
+
 
 
 console.log(document)
@@ -536,40 +348,8 @@ const tarjetasHtml = POKEMON.pokemon.reduce((acc, elemento, i) => {
             </div>
         `       
 },"")
-/*
-console.log(tarjetasHtml)
-
-const contenedorTarjetas = document.querySelector(".contenedor-tarjetas")
 
 
-console.log(document.forms)
-
-const cuadrado = document.querySelector(".cuadrado-rojo")
-console.log(cuadrado)
-
-
-
-const parrafo = document.querySelector("p")
-const botonVerMas = document.querySelector("#verMas")
-
-
-botonVerMas.addEventListener ("click", () => {
-    parrafo.classList.toggle("ver")
-
-    if (parrafo.classList.contains("ver")){
-        botonVerMas.textContent = "Cerrar"
-    } else {
-        botonVerMas.textContent = "Ver más"
-    }
-})
-
-
-
-cuadrado.onmousemove = () => {
-    console.log("me están clickendo")
-    cuadrado.classList.toggle("cuadrado-azul")
-}
-*/
 
 const inputUsuario = document.getElementById("usuario")
 const inputContrasenia = document.getElementById("contrasenia")
@@ -683,4 +463,125 @@ formNewNote.onsubmit = (e) => {
 
 let notasTraidasDelLocalStorage = valorDelLocalStorage("notas")
 notas = notasTraidasDelLocalStorage
- 
+ */
+ console.log ("librerias")
+
+ swal("sweet alert")
+
+ swal("Gracias por visitar esta pagina", "da click para continuar")
+
+ swal("Gracias por visitar esta pagina", "da click para continuar", "success")
+
+ const formularioNombre = document.querySelector("#form-nombre")
+
+ formularioNombre.onsubmit = (e) => {
+     e.preventDefault()
+     swal({
+         title: "Se ha enviado exitosamente",
+         text: "Haz click en continuar",
+         icon: "success",
+         button: "Continuar"
+     })
+ }
+ /*
+ swal({
+    title: "De que barrio es Huracan",
+    buttons: {
+    frase1: "Parque Patricios",
+    frase2: "Pompeya",
+    frase3: "Flores"
+    }
+ })
+*/
+const botonGeneradorDeNotis = document.querySelector("#noti-generator")
+console.log(botonGeneradorDeNotis)
+
+botonGeneradorDeNotis.onclick = () => {
+
+    Toastify({
+        text: "Aguante Huracan",
+        close: true,
+        duration: 6000
+    }).showToast()
+}
+
+const DateTime = luxon.DateTime
+console.log(DateTime)
+
+
+const fechaDeCumpleaños = DateTime.local(1992,4,21, 16, 10, 10, 5)
+console.log(fechaDeCumpleaños)
+
+
+console.log(fechaDeCumpleaños.toString())
+
+const diaDeHoy = DateTime.now()
+console.log(diaDeHoy)
+console.log(diaDeHoy.toLocaleString())
+
+
+const botonFechaActual = document.querySelector("#button-fecha")
+
+botonFechaActual.onclick = () => {
+ console.log(`${diaDeHoy.c.day}, ${diaDeHoy.c.month}, ${diaDeHoy.c.year}`)
+ document.querySelector(".caja").innerHTML = diaDeHoy.toLocaleString()
+}
+
+const fechaPorObjeto = DateTime.fromObject (
+    {
+        day: 15,
+        hour:13,
+        month: 5,
+        year: 1990
+    },
+    {
+        zona: "Parque Patricios/Buenos_Aires",
+        numberingSystem: 'beng'
+    }
+)
+
+console.log(fechaPorObjeto.toString())
+
+
+
+console.log(DateTime.fromISO("1999-10-01").toString())
+
+const fechaDeHoy = DateTime.now ()
+
+console.log(fechaDeHoy.year)
+console.log(fechaDeHoy.month)
+console.log(fechaDeHoy.day)
+
+console.log(fechaDeCumpleaños.zoneName)
+
+
+console.log(fechaDeHoy.toLocaleString(DateTime.DATE_HUGE))
+console.log(fechaDeHoy.toLocaleString(DateTime.DATE_SHORT)) 
+console.log(fechaDeHoy.toLocaleString(DateTime.DATE_FULL)) 
+
+
+
+const sumarDiasAFechaDeHoy = fechaDeHoy.plus({
+    hours: 1000000,
+    year: 1000,
+    second: 10000000,
+})
+
+console.log(sumarDiasAFechaDeHoy.toLocaleString(DateTime.DATE_HUGE))
+
+
+const restarDiasAFechaDeHoy = fechaDeHoy.minus({
+    year: 20000
+})
+
+console.log(restarDiasAFechaDeHoy.toLocaleString(DateTime.DATE_HUGE))
+
+
+const Interval = luxon.Interval
+
+const fechaAhoraYa = DateTime.now()
+const fechaDiezAniosAdelante = DateTime.local(2032, 08, 08)
+
+const intervaloEntreDosFechas = Interval.fromDateTimes(fechaAhoraYa, fechaDiezAniosAdelante)
+console.log(intervaloEntreDosFechas)
+console.log(intervaloEntreDosFechas.length("day"))
